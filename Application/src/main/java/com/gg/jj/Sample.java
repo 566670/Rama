1a6e6112097b06f468c64f6d5dc2f751621fa911
koti <kkr.kothapally@gmail.com>
2016-08-19 16:26:32 +0530

    COMMIT 1

---
+++ Application/src/main/java/com/gg/jj/Sample.java
@@ -0,0 +1,22 @@
+package com.gg.jj;
+
+public class Sample
+{
+public void m1()
+{
+	System.out.println("hai");
+}
+public void m2()
+{
+	System.out.println("hello");
+}
+public void m3()
+{
+}
+
+public void n()
+{
+	System.out.println("asasas");
+	System.out.println("asa");
+}
+}
